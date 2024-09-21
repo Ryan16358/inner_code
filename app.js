264d5d9717b79a59ce3adcafbf0a59c98b279730
Rayhan <153734474+Ryan16358@users.noreply.github.com>
2024-09-21 22:48:48 +0600

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
+const fileInput = document.querySelector("input"),
 
+downloadBtn = document.querySelector("button");
+
+downloadBtn.addEventListener("click", e => { e.preventDefault(); // preventing form from submitting fetchFile(fileInput.value);
+
+});
+
+
+function fetchFile(url) {
+
+// fetching file & returning response as blob
+
+fetch(url).then(res => res.blob()).then(file => {
+
+//URL.createObjURL creates a url of passed object
+
+let tempUrl = URL.createObjectURL(file);
+
+let aTag = document.createElement("a");
+
+aTag.href = tempUrl; // passing tempUrl as href value of <a> tag
+
+// passing file last name & extension as download value of <a> ta
+
+aTag.download = url.replace(/^.*[\\\/]/, '');
+
+document.body.appendChild(aTag); // adding <a> tag inside body
+
+aTag.click(); // clicking <a> tag so the file download
+
+aTag.remove(); // removing <a> tag once file downloaded
+
+URL.revokeObjectURL (tempUrl); // removing tempURL from the documen
+
+downloadBtn.innerText = "Download File";
+
+}).catch(() => {
+
+//
+
+downloadBtn.innerText = "Download File";
+
+alert("Failed to download file!");
+
+});
+
+}
